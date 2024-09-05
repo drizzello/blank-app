d3c6dfd7e197e3e8ee7ac04a92759375183d4120
Davide Rizzello <84180408+drizzello@users.noreply.github.com>
2024-09-05 11:08:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,17 @@ video_bytes = video_file.read()
 
 
 
-opzione_1 = "Opzione 1-1.pdf"
-opzione_2 = "Opzione 1-2.pdf"
-opzione_3 = "Opzione 1-3.pdf"
+opzione_1 = "Opzione 1-1-1.png"
+opzione_2 = "Opzione 1-2-1.png"
+opzione_3 = "Opzione 1-3-1.png"
 
 
-with open(opzione_1, "rb") as pdf_file:
-    document1 = pdf_file.read()
-with open(opzione_2, "rb") as pdf_file:
-    document2 = pdf_file.read()
-with open(opzione_3, "rb") as pdf_file:
-    document3 = pdf_file.read()
+with open(opzione_1, "rb") as file:
+    document1 = file.read()
+with open(opzione_2, "rb") as file:
+    document2 = file.read()
+with open(opzione_3, "rb") as file:
+    document3 = file.read()
 
 video, text = st.columns(2)
 
@@ -43,12 +43,12 @@ with text:
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        st.download_button("Opzione 1", document1, file_name="opzione1.pdf")
+        st.download_button("Opzione 1", document1, file_name="opzione1")
 
     with col2:
-        st.download_button("Opzione 2", document2, file_name="opzione2.pdf")
+        st.download_button("Opzione 2", document2, file_name="opzione2")
 
     with col3:
-        st.download_button("Opzione 3", document3, file_name="opzione3.pdf")
+        st.download_button("Opzione 3", document3, file_name="opzione3")
 
 
